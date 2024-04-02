9e0ec28a7b8e88d4964db77bf5de9a2ad297b5cc
Skriver <eaarlsk@students.eaaa.dk>
2024-04-02 22:48:53 +0200

    Initial commit, Template cleaning and creation of navigation bar

---
+++ rasmusskriver/src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import NavBar from './IntroPage/NavBar/navbar.jsx'
+import Main from './IntroPage/Main/main.jsx'
+import './App.css'
+// todo -> React Router
+function App() {
+  
+
+  return (
+    
+      <div>
+          <NavBar />
+          <Main />
+
+      </div>
+      
+  )
+}
+
+export default App
---
+++ rasmusskriver/src/IntroPage/Main/main.jsx
@@ -0,0 +1,13 @@
+function Main() {
+
+
+    return (
+
+        <div className="main">
+            <h1>Rasmus Skriver</h1>
+        </div>
+
+    )
+}
+
+export default Main
